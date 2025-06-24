2b3d3344f85d8d963d6c1d834b4d39553889c580
Usermah <usamaibrahim737.com>
2025-06-23 19:12:25 -0500

    Fully configured Cloudinary for media

--- project/urls.py
+++ project/urls.py
@@ -22,5 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),  # Include your app's URLs
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
+] 
 +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
